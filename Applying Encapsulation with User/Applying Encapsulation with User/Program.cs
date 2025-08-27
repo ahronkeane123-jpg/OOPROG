a0c9bd2e7dc956b88653ccffac2d4c78fa71ffc0
ACT-STUDENT <ACT-STUDENT@LAB3-PC09>
2025-08-27 10:40:28 +0800

    Encapsulation user input

---
+++ Applying Encapsulation with User/Applying Encapsulation with User/Program.cs
@@ -0,0 +1,35 @@
+﻿using Applying_Encapsulation_with_User.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Applying_Encapsulation_with_User
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("                          Welcome to the brand new shoe            ");
+            Console.WriteLine();
+            //Ask the user to input
+            Console.WriteLine("Enter your brand shoe: ");
+            string brand = Console.ReadLine();
+
+
+            Console.WriteLine("Enter your favorite color: ");
+            string color = Console.ReadLine();
+            Console.Clear();
+            //Initialized a method
+            Shoe shoe1 = new Shoe();
+            shoe1.brand = brand;
+            shoe1.color = color;
+
+            //Call the method
+            shoe1.Displayinfo();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Applying Encapsulation with User/Applying Encapsulation with User/Properties/Shoe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Applying_Encapsulation_with_User.Properties
+{
+    internal class Shoe
+    {
+        //Setting properties for the fields 
+        public string brand { get; set; }
+        public string color { get; set; }
+
+        //Method to display shoe information 
+        public void Displayinfo()
+        {
+            Console.WriteLine();
+            Console.WriteLine("             Brand of the shoe:     ");
+            Console.WriteLine();
+            Console.WriteLine("                     The Color:     ");
+            Console.ReadKey();
+
+        }
+    }
+}
