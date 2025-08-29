68d1b532548fae2e023787ab225fdc12c411d3af
ACT-STUDENT <ACT-STUDENT@LAB3-PC09>
2025-08-29 10:12:16 +0800

    Basics on Encapsulation

---
+++ PropertiesAndEncapsulation/PropertiesAndEncapsulation/Program.cs
@@ -0,0 +1,26 @@
+﻿using PropertiesAndEncapsulation.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesAndEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an instance of the Shoe class
+            Shoe shoe1 = new Shoe();
+
+            //Set properties using the setter methods 
+            shoe1.Brand = "Nike";
+            shoe1.Color = "Red";
+
+            //DisplayInfo method to print shoe1's details 
+            shoe1.DisplayInfo();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PropertiesAndEncapsulation/PropertiesAndEncapsulation/Properties/Shoe.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesAndEncapsulation.Properties
+{
+    internal class Shoe
+    {
+        private string brand;
+        private string color;
+
+        //Setting properties for the fields 
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        // Method to display shoe information 
+        public void DisplayInfo()
+        {
+            Console.Write("The basketball brand shoe is: " + Brand);
+            Console.WriteLine();
+            Console.Write("Color of the shoe: " + Color);
+            Console.ReadKey();
+        } 
+    }
+}
