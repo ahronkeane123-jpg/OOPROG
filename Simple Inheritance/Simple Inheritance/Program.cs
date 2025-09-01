c30ebf74ee083401eef9de4b2ad8ad958f523796
Papellero <ACT-STUDENT@LAB3-PC28>
2025-09-01 10:52:11 +0800

    Simple inheritance

---
+++ Simple Inheritance/Simple Inheritance/Program.cs
@@ -0,0 +1,29 @@
+﻿using Simple_Inheritance.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RegularStudent regularStudent = new RegularStudent();
+            {
+                regularStudent.Name = "Keane Hiligan";
+                regularStudent.Program = "BSIT";
+                regularStudent.Section = "IT301";
+            }
+            regularStudent.BasicInfo();
+            regularStudent.SectionEnrolled();
+
+            IrregularStudent irregularStudent1=new IrregularStudent();
+            {
+                irregularStudent1.Name = 
+            }
+        }
+    }
+}
---
+++ Simple Inheritance/Simple Inheritance/Properties/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Inheritance.Properties
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string Program { get; set; }
+        public void BasicInfo ()
+        {
+            Console.WriteLine("Name:" + Name);
+            Console.WriteLine("Program:" + Program);
+
+        }
+    }
+    class RegularStudent : Student
+    {
+        public string Section { get; set; }
+        public void SectionEnrolled()
+        {
+            Console.WriteLine("Section:" + Section);
+        }
+    }
+    class IrregularStudent : Student 
+    {
+        public string EnrolledSemUnits { get; set; }
+        public void EnrolledSemunits() 
+        {
+            Console.WriteLine("Units enrolled:" + EnrolledSemUnits);
+        }
+    }
+ }
+
