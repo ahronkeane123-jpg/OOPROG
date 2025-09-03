d96d23d04745f53fcdb09217c999649c41d896f3
sillarjohnwisley-afk <sillarjohnwisley@gmail.com>
2025-09-03 08:33:52 +0800

    Simple Inheritance

---
+++ Simple Inheritance/Simple Inheritance/Simple inheritance/Program.cs
@@ -0,0 +1,37 @@
+﻿using Simple_inheritance.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            // Create a RegularStudent object and set its properties
+            RegularStudent regularStudent = new RegularStudent()
+            {
+                Name = "Keane Hiligan",
+                Program = "BSIT",
+                Section = "IT301",
+            };
+            regularStudent.BasicInfo();// Display regular student info
+            regularStudent.SectionEnrolled();
+
+            // Create an IrregularStudent object and set its properties
+            IrregularStudent irregularStudent1 = new IrregularStudent()
+            {
+               Name = "Ahron Hiligan",
+               Program = "BSIT",
+               UnitsEnrolled = 18,
+            };
+            irregularStudent1.BasicInfo();// Display irregular student info
+            irregularStudent1.EnrolledSemunits();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Simple Inheritance/Simple Inheritance/Simple inheritance/Properties/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_inheritance.Properties
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string Program { get; set; }
+
+        public void BasicInfo()// Displays basic student info
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Program: " + Program);
+
+        }
+    }
+    // Derived class for regular students
+    class RegularStudent : Student
+    {
+        public string Section { get; set; }
+        public void SectionEnrolled()
+        {
+            Console.WriteLine("Section: " + Section);
+            Console.WriteLine();
+        }
+    }
+    // Derived class for irregular students
+    class IrregularStudent : Student
+    {
+        public int UnitsEnrolled { get; set; }
+        public void EnrolledSemunits()
+        {
+            Console.WriteLine("Units enrolled: " + UnitsEnrolled);
+        }
+    }
+}
