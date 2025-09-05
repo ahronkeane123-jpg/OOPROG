6b211751ad68eec2928484424026be95aadd4951
sillarjohnwisley-afk <sillarjohnwisley@gmail.com>
2025-09-05 10:49:41 +0800

    Constructor

---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display a prompt message to the console
+            Console.WriteLine("     Tell someone about yourself.     ");
+            Console.WriteLine();
+
+            // Create an instance of RegularStudent with name, course, and section
+            RegularStudent regularStudent1 = new RegularStudent("Edryl Dogomeo", "BSIT", "IT301");
+
+            // Call methods to display basic info and the section enrolled
+            regularStudent1.BasicInfo();
+            regularStudent1.SectionEnreolled();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // Create an instance of IrregularStudent with name, course, and number of enrolled units
+            IrregularStudent irregularStudent2 = new IrregularStudent("Keane Ahron Hiligan", "BSIT", 20);
+
+            // Call methods to display basic info and enrolled semester units
+            irregularStudent2.BasicInfo();
+            irregularStudent2.EnrolledSemUnits();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Constructor/Constructor/Student.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    // Base class representing a general student
+    internal class Student
+    {
+        // Fields to store student name and program
+        public string name;
+        public string program;
+
+
+        // Constructor to initialize the student's name and program
+        public Student(string aName, string aProgram)
+        {
+            name = aName;
+            program = aProgram;
+        }
+        // Method to display basic information about the student
+        public void BasicInfo()
+        {
+            Console.WriteLine("My name is: " + name + ". Taking the program. " + program + ".");
+
+        }
+ 
+    }
+    // Derived class representing a regular student
+
+    class RegularStudent : Student 
+    {
+        // Additional field to store section information
+        public string section;
+
+        public RegularStudent(string aName, string aProgram, string aSection) : base(aName, aProgram) 
+        {
+            section = aSection;
+        }
+        // Method to display the section and indicate student is regular
+        public void SectionEnreolled() 
+        {
+            Console.WriteLine(" Current section: " + section + ". I'm Regular Student.");
+        }
+    }
+    // Derived class representing an irregular student
+    class IrregularStudent : Student 
+    {
+        public int unitsEnrolled;
+
+        // Constructor to initialize name, program, and units enrolled
+        public IrregularStudent(string aName, string aProgram, int theUnitsEnrolled ) : base(aName, aProgram)
+        {
+            unitsEnrolled = theUnitsEnrolled;
+        }
+        // Method to display the number of units enrolled and indicate student is irregular
+
+        public void EnrolledSemUnits()
+        {
+            Console.WriteLine("Amount of the units I enrolled is: " + unitsEnrolled + ". I'm Irregular Student. ");
+        }
+
+    }
+        
+    
+}
+
