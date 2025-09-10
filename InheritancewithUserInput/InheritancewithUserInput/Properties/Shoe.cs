aad4690562b5d747f8f67ae2194dc8cdecb1c009
sillarjohnwisley-afk <sillarjohnwisley@gmail.com>
2025-09-10 10:51:52 +0800

    InheritancewithUserInput

---
+++ InheritancewithUserInput/InheritancewithUserInput/Properties/Shoe.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancewithUserInput.Properties
+{
+    class Shoe
+    {
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        
+        public void ShoeInfo()
+        {
+            Console.WriteLine(Brand + " " + Color);
+        }
+    }
+    class LowTopSneaker : Shoe
+    {
+        public string Type { get; set; }
+        public string Size { get; set; }
+        public void SneakerInfo()
+        {
+            ShoeInfo();
+            Console.WriteLine(Type + " " + Size);
+        }
+    }
+    class HighTop : Shoe
+    {
+        public string AnkleSupport { get; set; }
+        public string Material { get; set; }
+        public void HighTopInfo()
+        {
+            ShoeInfo();
+            Console.WriteLine(AnkleSupport + " " + Material);
+        }
+    }
+}
